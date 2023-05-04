0f30657f1e4e499ffd2fab6da9ccf30d6d8b9677
t3rrey <lachlanterrey@yahoo.com>
2023-05-05 08:23:47 +1000

    feat: add basic folder structure and npm packages

---
+++ src/lib/types/index.ts
@@ -0,0 +1,34 @@
+export type FoodItem = {
+    id: number;
+    name: string;
+    brand?: string;
+    category?: "meat" | "snack" | "drink";
+    protein: number;
+    carbs: number;
+    fats: number;
+    calories: number;
+  };
+  
+  export type MealPlanFoodITem = {
+    foodItem: FoodItem;
+    quantity: number;
+  };
+  
+  export type Meal = {
+    id: number;
+    name: string;
+    description: string;
+    foods: MealPlanFoodITem[];
+  };
+  
+  export type MealPlan = {
+    id: number;
+    trainer_id: number;
+    client_id: number;
+    name: string;
+    description: string;
+    meals: Meal[];
+  };
+  
+  
+  

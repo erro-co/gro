88023c93ed88b89117f2ae8911580c3566faa210
t3rrey <lachlanterrey@yahoo.com>
2023-05-05 12:19:57 +1000

    feat: add hooks and lint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier", "unused-imports"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "next/core-web-vitals",
+  ],
+  rules: {
+    "prettier/prettier": "error",
+    "comma-dangle": [2, "always-multiline"],
+    "unused-imports/no-unused-imports": "error",
+  },
+};
--- src/lib/supabase/index.ts
+++ src/lib/supabase/index.ts
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   "https://ogyelybkfudfsjarfonx.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9neWVseWJrZnVkZnNqYXJmb254Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzU3NTgyNTIsImV4cCI6MTk5MTMzNDI1Mn0._OdWSNG7J5bJuE-llxrDjAUG1undsckzxFGC-3pv12s"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9neWVseWJrZnVkZnNqYXJmb254Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzU3NTgyNTIsImV4cCI6MTk5MTMzNDI1Mn0._OdWSNG7J5bJuE-llxrDjAUG1undsckzxFGC-3pv12s",
 );

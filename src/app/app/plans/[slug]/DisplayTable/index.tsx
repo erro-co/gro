c0d74121a1ebe2c801e939d95fc1b9ef2993b6a9
Lachlan <lachlan@lachlans-mbp.lan>
2023-09-08 14:14:35 +1000

    feat: redorder table and styling

--- src/app/app/plans/[slug]/DisplayTable/index.tsx
+++ src/app/app/plans/[slug]/DisplayTable/index.tsx
@@ -1,6 +1,8 @@
+import { capitalizeFirstLetter } from "@/lib/helpers";
 import useMediaQuery from "@/lib/hooks/useMediaQuery";
 import { Disclosure } from "@headlessui/react";
 import { ChevronUpIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
 import { FC } from "react";
 import { MealFoodServing } from "../page";
 
@@ -23,63 +25,68 @@ const DisplayTable: FC<IDisplayTable> = ({ foods }) => {
   const isMobile = useMediaQuery("(max-width: 640px)");
   return (
     <div className="pb-12">
-      <h2 className="font-semibold">{foods[0].meal.name}</h2>
-      <div className="mt-2 border border-gray-300 rounded-lg p-1">
+      <h2 className="font-semibold mb-2 text-xl">
+        {capitalizeFirstLetter(foods[0].meal.name)}
+      </h2>
+      <div className="border border-gro-pink rounded-lg">
         <table className="min-w-full divide-y divide-gray-300">
-          <thead>
-            <tr>
+          <thead className="bg-gro-pink">
+            <tr className="text-white rounded-t-lg">
               <th
                 scope="col"
-                className="py-2 pl-2 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                className="w-[200px] py-2 pl-2 pr-3 text-left text-sm font-semibold rounded-tl-lg"
               >
                 Food
               </th>
               <th
                 scope="col"
-                className="hidden px-3 py-2 text-left text-sm font-semibold text-gray-900 sm:table-cell"
+                className="px-3 py-2 text-left text-sm font-semibold "
               >
-                Brand
+                Servings
               </th>
               <th
                 scope="col"
-                className="hidden px-3 py-2 text-left text-sm font-semibold text-gray-900 lg:table-cell"
-              >
-                Protein (g)
-              </th>
-              <th
-                scope="col"
-                className="hidden px-3 py-2 text-left text-sm font-semibold text-gray-900 lg:table-cell"
-              >
-                Fats (g)
-              </th>
-              <th
-                scope="col"
-                className="hidden px-3 py-2 text-left text-sm font-semibold text-gray-900 lg:table-cell"
-              >
-                Carbs (g)
-              </th>
-              <th
-                scope="col"
-                className="px-3 py-2 text-left text-sm font-semibold text-gray-900"
+                className={clsx(
+                  "px-3 py-2 text-left text-sm font-semibold",
+                  isMobile && "rounded-tr-lg",
+                )}
               >
                 Calories {isMobile ? null : "(kcal)"}
               </th>
               <th
                 scope="col"
-                className="px-3 py-2 text-left text-sm font-semibold text-gray-900"
+                className="hidden px-3 py-2 text-left text-sm font-semibold lg:table-cell"
               >
-                Servings
+                Protein (g)
+              </th>
+              <th
+                scope="col"
+                className="hidden px-3 py-2 text-left text-sm font-semibold lg:table-cell"
+              >
+                Fats (g)
+              </th>
+              <th
+                scope="col"
+                className="hidden px-3 py-2 text-left text-sm font-semibold lg:table-cell rounded-tr-lg"
+              >
+                Carbs (g)
               </th>
             </tr>
           </thead>
           <tbody className="divide-y divide-gray-200 bg-white">
             {foods.map((food: MealFoodServing, idx) => (
               <tr key={idx} className="font-light text-xs lg:text-sm">
-                <td className="whitespace-nowrap py-2 pl-2 pr-3  text-gray-900 sm:pl-0">
-                  {food.food.name}
+                <td className=" w-[600px] whitespace-nowrap py-2 pr-3 pl-2">
+                  <p>{food.food.name}</p>
+                  <p className="text-gray-400 text-xs">{food.food.brand}</p>
                 </td>
-                <td className="hidden whitespace-nowrap px-3 py-1  text-gray-500 lg:table-cell">
-                  {food.food.brand}
+                <td className="whitespace-nowrap px-3 py-1 text-gray-500 lg:table-cell">
+                  {food.quantity} x {food.serving.name} {"("}
+                  {Number(food.serving.weight * food.quantity).toFixed()}
+                  {"g)"}
+                </td>
+                <td className="whitespace-nowrap px-3 py-1 text-gray-500 sm:table-cell">
+                  {food.food.nutrients.calories * food.quantity}
                 </td>
                 <td className="hidden whitespace-nowrap px-3 py-1  text-gray-500 lg:table-cell">
                   {food.food.nutrients.protein * food.quantity}
@@ -90,20 +97,17 @@ const DisplayTable: FC<IDisplayTable> = ({ foods }) => {
                 <td className="hidden whitespace-nowrap px-3 py-1 text-gray-500 lg:table-cell">
                   {food.food.nutrients.total_carbs * food.quantity}
                 </td>
-                <td className="whitespace-nowrap px-3 py-1 text-gray-500 sm:table-cell">
-                  {food.food.nutrients.calories * food.quantity}
-                </td>
-                <td className="whitespace-nowrap px-3 py-1 text-gray-500 lg:table-cell">
-                  {food.quantity} {food.serving.name}
-                </td>
               </tr>
             ))}
-            <tr className="text-xs lg:text-sm">
+            <tr className="text-xs lg:text-sm rounded-b-lg">
               <td
-                className="whitespace-nowrap py-2 pl-2 pr-3 font-bold text-gray-900 sm:pl-0"
-                colSpan={isMobile ? 1 : 2}
+                className="whitespace-nowrap pl-2 py-2 pr-3 font-bold text-gray-900 rounded-bl-lg"
+                colSpan={2}
               >
                 Totals:
+              </td>
+              <td className="whitespace-nowrap px-3 py-1 font-bold rounded-br-lg">
+                <p>{getTotalMacro(foods, "calories")}</p>
               </td>
               <td className="hidden whitespace-nowrap px-3 py-1  font-bold lg:table-cell">
                 <p>{getTotalMacro(foods, "protein")}</p>
@@ -111,11 +115,8 @@ const DisplayTable: FC<IDisplayTable> = ({ foods }) => {
               <td className="hidden lg:table-cell whitespace-nowrap px-3 py-1 font-bold">
                 <p>{getTotalMacro(foods, "total_fat")}</p>
               </td>
-              <td className="hidden lg:table-cell whitespace-nowrap px-3 py-1 font-bold">
+              <td className="hidden lg:table-cell whitespace-nowrap px-3 py-2 font-bold rounded-br-lg">
                 <p>{getTotalMacro(foods, "total_carbs")}</p>
-              </td>
-              <td className="whitespace-nowrap px-3 py-1 font-bold">
-                <p>{getTotalMacro(foods, "calories")}</p>
               </td>
             </tr>
           </tbody>

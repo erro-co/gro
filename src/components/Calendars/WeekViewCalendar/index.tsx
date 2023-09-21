f47622cc20d759cc45292a70cdb1c7efb8ccf0b3
Lachlan <lachlan@lachlans-mbp.lan>
2023-09-21 10:16:27 +1000

    feat: calendar draft 2

--- src/components/Calendars/WeekViewCalendar/index.tsx
+++ src/components/Calendars/WeekViewCalendar/index.tsx
@@ -1,15 +1,86 @@
-import { FC, useEffect, useRef } from "react";
+"use client";
+import { FC, Fragment, useEffect, useRef, useState } from "react";
+import { Appointment } from "..";
 
 export interface IWeekViewCalendar {
   currentDate?: Date;
   selectedDate?: Date;
-  appointments?: any[];
+  appointments?: Appointment[];
 }
 
-const WeekViewCalendar: FC<IWeekViewCalendar> = () => {
+const currentDateClass =
+  "ml-1.5 flex h-8 w-8 rounded-full bg-indigo-600 font-semibold text-white";
+
+const Event: FC<{ appointment: Appointment }> = ({ appointment }) => {
+  const convertToAEST = (dateString: string) => {
+    const date = new Date(dateString);
+    return new Intl.DateTimeFormat("en-AU", {
+      timeZone: "Australia/Sydney", // Sydney falls under AEST
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    }).format(date);
+  };
+
+  const getGridRowStart = (time: string) => {
+    const [hours, minutes] = time.split(":");
+    return 2 + parseInt(hours, 10) * 12 + Math.floor(parseInt(minutes, 10) / 5);
+  };
+  const appointmentDay = new Date(appointment.startAt).getDay();
+  console.log(appointmentDay);
+  const startTimeAEST = convertToAEST(appointment.startAt);
+  const gridRowStart = getGridRowStart(startTimeAEST);
+  const gridRowSpan = Math.ceil(appointment.durationMinutes / 2);
+  const gridColumnStart = appointmentDay === 0 ? 7 : appointmentDay;
+
+  return (
+    <li
+      className={`relative mt-px flex col-start-${gridColumnStart}`}
+      style={{ gridRow: `${gridRowStart} / span ${gridRowSpan}` }}
+    >
+      <a
+        href="#"
+        className="group absolute inset-1 flex flex-col overflow-y-auto rounded-lg bg-blue-50 p-2 text-xs leading-5 hover:bg-blue-100"
+      >
+        <p className="order-1 font-semibold text-blue-700">Training</p>
+        <p className="text-blue-500 group-hover:text-blue-700">
+          <time dateTime={appointment.startAt}>{startTimeAEST}</time>
+        </p>
+      </a>
+    </li>
+  );
+};
+const getWeekDates = (date: Date): Date[] => {
+  const startDate = new Date(date);
+  const day = startDate.getDay();
+
+  // If it's Sunday, set to previous Monday
+  if (day === 0) {
+    startDate.setDate(date.getDate() - 6);
+  }
+  // Else set to the previous Monday of the current week
+  else {
+    startDate.setDate(date.getDate() - (day - 1));
+  }
+
+  const dates = [startDate];
+
+  for (let i = 1; i < 7; i++) {
+    const nextDate = new Date(startDate);
+    nextDate.setDate(startDate.getDate() + i);
+    dates.push(nextDate);
+  }
+  return dates;
+};
+
+const WeekViewCalendar: FC<IWeekViewCalendar> = ({
+  currentDate,
+  appointments,
+}) => {
   const container = useRef<HTMLDivElement>(null);
   const containerNav = useRef<HTMLDivElement>(null);
   const containerOffset = useRef<HTMLDivElement>(null);
+  const [weekDates, setWeekDates] = useState<Date[]>();
 
   useEffect(() => {
     // Set the container scroll position based on the current time.
@@ -22,6 +93,10 @@ const WeekViewCalendar: FC<IWeekViewCalendar> = () => {
           containerOffset.current.offsetHeight) *
           currentMinute) /
         1440;
+    }
+
+    if (currentDate) {
+      setWeekDates(getWeekDates(currentDate));
     }
   }, []);
 
@@ -39,285 +114,57 @@ const WeekViewCalendar: FC<IWeekViewCalendar> = () => {
           className="sticky top-0 z-30 flex-none bg-white shadow ring-1 ring-black ring-opacity-5 sm:pr-8"
         >
           <div className="grid grid-cols-7 text-sm leading-6 text-gray-500 sm:hidden">
-            <button
-              type="button"
-              className="flex flex-col items-center pb-3 pt-2"
-            >
-              M{" "}
-              <span className="mt-1 flex h-8 w-8 items-center justify-center font-semibold text-gray-900">
-                10
-              </span>
-            </button>
-            <button
-              type="button"
-              className="flex flex-col items-center pb-3 pt-2"
-            >
-              T{" "}
-              <span className="mt-1 flex h-8 w-8 items-center justify-center font-semibold text-gray-900">
-                11
-              </span>
-            </button>
-            <button
-              type="button"
-              className="flex flex-col items-center pb-3 pt-2"
-            >
-              W{" "}
-              <span className="mt-1 flex h-8 w-8 items-center justify-center rounded-full bg-indigo-600 font-semibold text-white">
-                12
-              </span>
-            </button>
-            <button
-              type="button"
-              className="flex flex-col items-center pb-3 pt-2"
-            >
-              T{" "}
-              <span className="mt-1 flex h-8 w-8 items-center justify-center font-semibold text-gray-900">
-                13
-              </span>
-            </button>
-            <button
-              type="button"
-              className="flex flex-col items-center pb-3 pt-2"
-            >
-              F{" "}
-              <span className="mt-1 flex h-8 w-8 items-center justify-center font-semibold text-gray-900">
-                14
-              </span>
-            </button>
-            <button
-              type="button"
-              className="flex flex-col items-center pb-3 pt-2"
-            >
-              S{" "}
-              <span className="mt-1 flex h-8 w-8 items-center justify-center font-semibold text-gray-900">
-                15
-              </span>
-            </button>
-            <button
-              type="button"
-              className="flex flex-col items-center pb-3 pt-2"
-            >
-              S{" "}
-              <span className="mt-1 flex h-8 w-8 items-center justify-center font-semibold text-gray-900">
-                16
-              </span>
-            </button>
+            {weekDates?.map((date, idx) => (
+              <button
+                key={idx}
+                type="button"
+                className="flex flex-col items-center pb-3 pt-2"
+              >
+                {["S", "M", "T", "W", "T", "F", "S"][date.getDay()]}{" "}
+                <span
+                  className={`mt-1 flex h-8 w-8 items-center justify-center ${
+                    currentDate &&
+                    currentDate.toDateString() === date.toDateString()
+                      ? currentDateClass
+                      : "font-semibold text-gray-900"
+                  }`}
+                >
+                  {date.getDate()}
+                </span>
+              </button>
+            ))}
           </div>
 
           <div className="-mr-px hidden grid-cols-7 divide-x divide-gray-100 border-r border-gray-100 text-sm leading-6 text-gray-500 sm:grid">
             <div className="col-end-1 w-14" />
-            <div className="flex items-center justify-center py-3">
-              <span>
-                Mon{" "}
-                <span className="items-center justify-center font-semibold text-gray-900">
-                  10
+            {weekDates?.map((date, idx) => (
+              <div key={idx} className="flex items-center justify-center py-3">
+                <span className="flex items-baseline">
+                  {
+                    ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"][
+                      date.getDay()
+                    ]
+                  }
+                  <span
+                    className={`ml-1 items-center justify-center ${
+                      currentDate &&
+                      currentDate.toDateString() === date.toDateString()
+                        ? currentDateClass
+                        : "font-semibold text-gray-900 "
+                    }`}
+                  >
+                    {date.getDate()}
+                  </span>
                 </span>
-              </span>
-            </div>
-            <div className="flex items-center justify-center py-3">
-              <span>
-                Tue{" "}
-                <span className="items-center justify-center font-semibold text-gray-900">
-                  11
-                </span>
-              </span>
-            </div>
-            <div className="flex items-center justify-center py-3">
-              <span className="flex items-baseline">
-                Wed{" "}
-                <span className="ml-1.5 flex h-8 w-8 items-center justify-center rounded-full bg-indigo-600 font-semibold text-white">
-                  12
-                </span>
-              </span>
-            </div>
-            <div className="flex items-center justify-center py-3">
-              <span>
-                Thu{" "}
-                <span className="items-center justify-center font-semibold text-gray-900">
-                  13
-                </span>
-              </span>
-            </div>
-            <div className="flex items-center justify-center py-3">
-              <span>
-                Fri{" "}
-                <span className="items-center justify-center font-semibold text-gray-900">
-                  14
-                </span>
-              </span>
-            </div>
-            <div className="flex items-center justify-center py-3">
-              <span>
-                Sat{" "}
-                <span className="items-center justify-center font-semibold text-gray-900">
-                  15
-                </span>
-              </span>
-            </div>
-            <div className="flex items-center justify-center py-3">
-              <span>
-                Sun{" "}
-                <span className="items-center justify-center font-semibold text-gray-900">
-                  16
-                </span>
-              </span>
-            </div>
+              </div>
+            ))}
           </div>
         </div>
         <div className="flex flex-auto">
           <div className="sticky left-0 z-10 w-14 flex-none bg-white ring-1 ring-gray-100" />
           <div className="grid flex-auto grid-cols-1 grid-rows-1">
             {/* Horizontal lines */}
-            <div
-              className="col-start-1 col-end-2 row-start-1 grid divide-y divide-gray-100"
-              style={{ gridTemplateRows: "repeat(48, minmax(3.5rem, 1fr))" }}
-            >
-              <div ref={containerOffset} className="row-end-1 h-7"></div>
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  12AM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  1AM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  2AM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  3AM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  4AM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  5AM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  6AM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  7AM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  8AM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  9AM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  10AM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  11AM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  12PM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  1PM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  2PM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  3PM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  4PM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  5PM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  6PM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  7PM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  8PM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  9PM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  10PM
-                </div>
-              </div>
-              <div />
-              <div>
-                <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
-                  11PM
-                </div>
-              </div>
-              <div />
-            </div>
+            <TimeSlots />
 
             {/* Vertical lines */}
             <div className="col-start-1 col-end-2 row-start-1 hidden grid-cols-7 grid-rows-1 divide-x divide-gray-100 sm:grid sm:grid-cols-7">
@@ -338,58 +185,61 @@ const WeekViewCalendar: FC<IWeekViewCalendar> = () => {
                 gridTemplateRows: "1.75rem repeat(288, minmax(0, 1fr)) auto",
               }}
             >
-              <li
-                className="relative mt-px flex sm:col-start-3"
-                style={{ gridRow: "74 / span 12" }}
-              >
-                <a
-                  href="#"
-                  className="group absolute inset-1 flex flex-col overflow-y-auto rounded-lg bg-blue-50 p-2 text-xs leading-5 hover:bg-blue-100"
-                >
-                  <p className="order-1 font-semibold text-blue-700">
-                    Breakfast
-                  </p>
-                  <p className="text-blue-500 group-hover:text-blue-700">
-                    <time dateTime="2022-01-12T06:00">6:00 AM</time>
-                  </p>
-                </a>
-              </li>
-              <li
-                className="relative mt-px flex sm:col-start-3"
-                style={{ gridRow: "92 / span 30" }}
-              >
-                <a
-                  href="#"
-                  className="group absolute inset-1 flex flex-col overflow-y-auto rounded-lg bg-pink-50 p-2 text-xs leading-5 hover:bg-pink-100"
-                >
-                  <p className="order-1 font-semibold text-pink-700">
-                    Flight to Paris
-                  </p>
-                  <p className="text-pink-500 group-hover:text-pink-700">
-                    <time dateTime="2022-01-12T07:30">7:30 AM</time>
-                  </p>
-                </a>
-              </li>
-              <li
-                className="relative mt-px hidden sm:col-start-6 sm:flex"
-                style={{ gridRow: "122 / span 24" }}
-              >
-                <a
-                  href="#"
-                  className="group absolute inset-1 flex flex-col overflow-y-auto rounded-lg bg-gray-100 p-2 text-xs leading-5 hover:bg-gray-200"
-                >
-                  <p className="order-1 font-semibold text-gray-700">
-                    Meeting with design team at Disney
-                  </p>
-                  <p className="text-gray-500 group-hover:text-gray-700">
-                    <time dateTime="2022-01-15T10:00">10:00 AM</time>
-                  </p>
-                </a>
-              </li>
+              {appointments?.map((appointment, idx) => (
+                <Event key={idx} appointment={appointment} />
+              ))}
             </ol>
           </div>
         </div>
       </div>
+    </div>
+  );
+};
+
+const TimeSlots: FC = () => {
+  const times = [
+    "12AM",
+    "1AM",
+    "2AM",
+    "3AM",
+    "4AM",
+    "5AM",
+    "6AM",
+    "7AM",
+    "8AM",
+    "9AM",
+    "10AM",
+    "11AM",
+    "12PM",
+    "1PM",
+    "2PM",
+    "3PM",
+    "4PM",
+    "5PM",
+    "6PM",
+    "7PM",
+    "8PM",
+    "9PM",
+    "10PM",
+    "11PM",
+  ];
+
+  return (
+    <div
+      className="col-start-1 col-end-2 row-start-1 grid divide-y divide-gray-100"
+      style={{ gridTemplateRows: "repeat(48, minmax(3.5rem, 1fr))" }}
+    >
+      <div className="row-end-1 h-7"></div>
+      {times.map((time, index) => (
+        <Fragment key={index}>
+          <div>
+            <div className="sticky left-0 z-20 -ml-14 -mt-2.5 w-14 pr-2 text-right text-xs leading-5 text-gray-400">
+              {time}
+            </div>
+          </div>
+          <div />
+        </Fragment>
+      ))}
     </div>
   );
 };
--- src/components/Calendars/index.tsx
+++ src/components/Calendars/index.tsx
@@ -6,12 +6,77 @@ import CalendarHeader from "./CalendarHeader";
 import WeekViewCalendar from "./WeekViewCalendar";
 
 export type CalendarViews = "day" | "week" | "month" | "year";
+export type SquareAppointmentResponse = {
+  id: string;
+  version: number;
+  status: "CANCELLED_BY_SELLER" | "ACCEPTED"; // Add other statuses if available
+  createdAt: string; // Consider using Date if you'll parse the string
+  updatedAt: string; // Consider using Date if you'll parse the string
+  startAt: string; // Consider using Date if you'll parse the string
+  locationId: string;
+  customerId: string;
+  appointmentSegments: AppointmentSegment[];
+  transitionTimeMinutes: number;
+  allDay: boolean;
+  locationType: "BUSINESS_LOCATION"; // Add other types if available
+  creatorDetails: CreatorDetails;
+  source: "FIRST_PARTY_MERCHANT"; // Add other sources if available
+};
+
+type AppointmentSegment = {
+  durationMinutes: number;
+  serviceVariationId: string;
+  teamMemberId: string;
+  serviceVariationVersion: string;
+  intermissionMinutes: number;
+  anyTeamMember: boolean;
+};
+
+type CreatorDetails = {
+  creatorType: "TEAM_MEMBER"; // Add other types if available
+  teamMemberId: string;
+};
+
+export type Appointment = {
+  id: string;
+  status: "CANCELLED_BY_SELLER" | "ACCEPTED";
+  startAt: string;
+  endAt: string;
+  durationMinutes: number;
+};
+
+function mapAppointments(
+  appointments: SquareAppointmentResponse[],
+): Appointment[] {
+  return appointments.map((appointment) => {
+    // Sum the duration from each segment
+    const totalDuration = appointment.appointmentSegments.reduce(
+      (sum, segment) => sum + segment.durationMinutes,
+      0,
+    );
+
+    // Convert startAt to Date and add totalDuration
+    const startDate = new Date(appointment.startAt);
+    const endDate = new Date(startDate.getTime() + totalDuration * 60000); // 60000ms = 1 minute
+
+    // Convert endDate back to string format
+    const endAt = endDate.toISOString(); // Assuming you want ISO string format
+
+    return {
+      id: appointment.id,
+      status: appointment.status,
+      startAt: appointment.startAt,
+      endAt: endAt,
+      durationMinutes: totalDuration,
+    };
+  });
+}
 
 export default function Calendar() {
   const [calendarView, setCalendarView] = useState<CalendarViews>("week");
   const [date, setDate] = useState<Date>(new Date());
   const [selectedDate, setSelectedDate] = useState<Date>(new Date());
-  const [appointments, setAppointments] = useState<any[]>([]);
+  const [appointments, setAppointments] = useState<Appointment[]>([]);
 
   const supabase = createClientComponentClient<Database>();
 
@@ -22,7 +87,9 @@ export default function Calendar() {
     const data = await axios.get(
       `https://gro-api.vercel.app/bookings?q=${user?.email}`,
     );
-    console.log(data.data.bookings);
+    const mappedAppointments = mapAppointments(data.data.bookings);
+    console.log(mappedAppointments);
+    setAppointments(mappedAppointments);
   };
 
   useEffect(() => {
@@ -30,20 +97,24 @@ export default function Calendar() {
   }, []);
 
   return (
-    <div className="p-2 border rounded-lg">
-      <CalendarHeader />
-      <div>
-        {calendarView === "day" && <div>Day view</div>}
-        {calendarView === "week" && (
-          <WeekViewCalendar
-            currentDate={date}
-            selectedDate={selectedDate}
-            appointments={appointments}
-          />
-        )}
-        {calendarView === "month" && <div>Month view</div>}
-        {calendarView === "year" && <div>Year view</div>}
+    <>
+      <h2 className="my-4">My total bookings left: {appointments.length}</h2>
+      <div className="p-2 border rounded-lg">
+        <CalendarHeader />
+
+        <div>
+          {calendarView === "day" && <div>Day view</div>}
+          {calendarView === "week" && (
+            <WeekViewCalendar
+              currentDate={date}
+              selectedDate={selectedDate}
+              appointments={appointments}
+            />
+          )}
+          {calendarView === "month" && <div>Month view</div>}
+          {calendarView === "year" && <div>Year view</div>}
+        </div>
       </div>
-    </div>
+    </>
   );
 }

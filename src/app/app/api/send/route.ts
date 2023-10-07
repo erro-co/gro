fbbc9d806030e218f0c239d1b4ec229d0fcc1d52
Lachlan <lachlan@lachlans-mbp.lan>
2023-10-08 10:06:51 +1100

    feat: lock file

--- src/app/app/api/send/route.ts
+++ src/app/app/api/send/route.ts
@@ -9,26 +9,28 @@ export async function POST(request: NextRequest) {
     // const { planId, firstName, trainerFirstName, clientEmail } = body;
     const { data } = await request.json();
 
-    // // Check if the required parameters are provided
-    // if (
-    //   typeof planId === "undefined" ||
-    //   typeof firstName === "undefined" ||
-    //   typeof trainerFirstName === "undefined" ||
-    //   typeof clientEmail === "undefined"
-    // ) {
-    //   return NextResponse.json({ error: "Required parameters missing" });
-    // }
+        // from: "Gro Team <team@grohealth.co>",
+      // to: [data.clientEmail],
+      // subject: "GRO: You have a new Meal Plan available!",
+      // react: NotifyNewPlanEmail({
+      //   firstName: data.firstName,
+      //   planId: data.planId,
+      //   trainerFirstName: data.trainerFirstName,
+      // }),
+    
 
-    const email = await resend.emails.send({
-      from: "Gro Team <team@grohealth.co>",
-      to: [data.clientEmail],
-      subject: "GRO: You have a new Meal Plan available!",
-      react: NotifyNewPlanEmail({
-        firstName: data.firstName,
-        planId: data.planId,
-        trainerFirstName: data.trainerFirstName,
-      }),
-    });
+      const email = await resend.emails.send({
+        from: "Gro Team <team@grohealth.co>",
+        to: [data.clientEmail],
+        subject: "GRO: You have a new Meal Plan available!",
+        text: "Your new meal plan is now available. Please check the attached details.", 
+        react: NotifyNewPlanEmail({
+          firstName: data.firstName,
+          planId: data.planId,
+          trainerFirstName: data.trainerFirstName,
+        }),
+      });
+      
 
     return NextResponse.json(email);
   } catch (error) {
